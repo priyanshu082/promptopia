10e8d425254e843682fecd62286fed10719ce3e4
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-13 23:13:40 +0530

    some ui is fixed

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Form = ({type,post,setPost,submitting,handleSubmit}) => {
   return (
-    <section className='w-full flex flex-col justify-center items-center min-h-[800px] px-[20px]'>
+    <section className='w-full z-[1000] flex flex-col justify-center items-center min-h-[800px] px-[20px]'>
       <h1 className="head_text text-left">
         <span className="blue_gradient ">
           {type} Post
