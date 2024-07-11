ef2ccb1bdde638e86129098b93c5f5b2b84a1fae
priyanshu082 <priyanshusingh216@gmail.com>
2024-07-11 16:17:04 +0530

    fetch post request is shifted to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,27 @@
+"use client"
 import Feed from "@/components/Feed";
 import { TextRevealCard } from "@/components/ui/Reveal";
+import { useEffect,useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]) 
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await axios.get(`/api/prompt`)
+        console.log(response)
+        const data = response.data
+        const reversedData = [...data].reverse()
+        if(response){console.log(reversedData)}     
+        setPosts(reversedData)
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    }
+    fetchPost();
+  }, [])
+
   return (
     <section className="  w-[100vw] overflow-clip flex-center flex-col ">
       
@@ -10,7 +30,7 @@ const Home = () => {
         <TextRevealCard revealText="AI Prompts" text="AI Prompts" />
       </div>
 
-      <Feed />
+      <Feed posts={posts}/>
     </section>
   );
 };
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,32 +5,12 @@ import { HoverEffect } from "./ui/card-hover-effect"
 import axios from "axios"
 
 
-const Feed = () => {
+const Feed = ({posts}) => {
 
     const [searchText, setSearchText] = useState('')
-    const [posts, setPosts] = useState([]) 
 
-    useEffect(() => {
-      const fetchPost = async () => {
-        try {
-          const response = await axios.get(`/api/prompt`)
-          console.log(response)
-          const data = response.data
-          const reversedData = [...data].reverse()
-          if(response){console.log(reversedData)}     
-          setPosts(reversedData)
-        } catch (error) {
-          console.error('Fetch error:', error);
-        }
-      }
-      fetchPost();
-    }, [])
-
-    
   return (
     <section className="mt-16 mx-auto w-full flex justify-start items-center flex-col gap-2 min-h-[800px]">
-    
-
     <HoverEffect items={posts}/>
     </section>
   )
